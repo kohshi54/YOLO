4c148f0d659322713453f6d5252f1ebe7f30c681
Kohshi.Y <80312261+kohshi54@users.noreply.github.com>
2023-09-30 14:02:11 +0900

    Update tb_Convolution3x3.v

--- tb_Convolution3x3.v
+++ tb_Convolution3x3.v
@@ -28,8 +28,30 @@ module tb_Convolution3x3();
         rst_n = 0;
         #10 rst_n = 1;
 
-        // Set input_patch, kernel, and bias for a test case
-        // ... (you'd provide values here)
+        // Set input_patch values
+        input_patch[0][0] = 8'd10;  // Assigning a value of 10 to the element at (0,0)
+        input_patch[0][1] = 8'd15;
+        input_patch[0][2] = 8'd20;
+        input_patch[1][0] = 8'd25;
+        input_patch[1][1] = 8'd30;
+        input_patch[1][2] = 8'd35;
+        input_patch[2][0] = 8'd40;
+        input_patch[2][1] = 8'd45;
+        input_patch[2][2] = 8'd50;
+
+        // Set kernel values
+        kernel[0][0] = 8'd2; 
+        kernel[0][1] = 8'd2; 
+        kernel[0][2] = 8'd2; 
+        kernel[1][0] = 8'd2; 
+        kernel[1][1] = 8'd2; 
+        kernel[1][2] = 8'd2; 
+        kernel[2][0] = 8'd2; 
+        kernel[2][1] = 8'd2; 
+        kernel[2][2] = 8'd2; 
+
+        // Set bias value
+        bias = 8'd1; 
 
         #10;
         $display("Output pixel: %d", output_pixel);
